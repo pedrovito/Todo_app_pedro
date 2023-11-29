970c5eabe809cf8bb5ffa5676fbb74d8395bbd73
pedrovito <pedro.v.oliveira007@gmail.com>
2023-11-29 08:25:38 -0300

    salvando a tarefa no banco de dados

--- index.js
+++ index.js
@@ -6,12 +6,35 @@ const mysql = require("mysql2")
 const exphbs = require("express-handlebars")
 
 const app = express()
-app.use
+
 
 app.engine('handlebars',exphbs.engine())
 app.set('view engine','handlebars')
 
 app.use(express.static('public'))
+
+app.use(express.urlencoded({
+    extended:true
+}))
+
+app.use(express.json())
+
+//rotas
+app.post('/criar', (requesicao, resposta) =>{
+    const descricao =requesicao.body.descricao
+    const completa = 0
+    const sql =`
+            INSERT INTO tarefas(descricao, completa)
+            VALUE ('${descricao}','${completa}' )
+            `
+        conexao.query(sql,(erro)=>{
+            if (erro){
+                return console.log(erro)
+            }
+
+            resposta.redirect('/')
+        })
+    })
 
 app.get('/',(requesicao,resposta)=>{
     resposta.render('home')
@@ -31,7 +54,7 @@ conexao.connect((erro)=>{
     }
 
     console.log("estou no mysql")
-    
+
     app.listen(3000,() =>{
         console.log("servido rodando na porta 3000")
     })  
